9d62871f8c9015e2535a89c1d8dd335e34c143fd
SergeyRe <38654146+SergeyRe@users.noreply.github.com>
2021-02-02 12:06:09 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -6,14 +6,14 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods:  ['POST'],
-    origin:"*"
+   
   })
 )
 
 export default async function handler(req, res) {
   // Run cors
-//  await cors(req, res)
+  console.log(req)
+  await cors(req, res)
 
     
  
