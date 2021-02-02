47bf483c65e199feb5bf964eee73fc43bd1d0328
SergeyRe <38654146+SergeyRe@users.noreply.github.com>
2021-02-02 08:13:59 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
       res.status(200).json({ message: 'Hello Everyone!' })
       resolve();
-  }
+  })
 
   
 
